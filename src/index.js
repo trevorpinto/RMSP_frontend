5f6c5be8b90f3323bbde01fccf6da487ca882434
Trevor Pinto <74032637+roverTunleashed@users.noreply.github.com>
2025-06-14 20:45:06 -0700

    remove more old code, start new rebuild of header

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import { BrowserRouter } from 'react-router-dom'; // <--- MUST BE IMPORTED
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Ensure your App component (and therefore all its children) is inside BrowserRouter */}
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
