3b3bb0c61eed77ebbd933985b50c9e2ff47805ce
Trevor Pinto <74032637+roverTunleashed@users.noreply.github.com>
2025-06-21 19:46:20 -0700

    updated file structure

--- src/HomePage/Header.js
+++ src/HomePage/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import image from '../RMSP.png'
 
 function DisplayHeader(){
     return <>
-        <img src = ''/>
+        <img src = {image}/>
         <span>About</span>
         <h1>Learn More</h1>
     </>
