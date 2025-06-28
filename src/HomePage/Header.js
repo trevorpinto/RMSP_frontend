5b26ee37a49c6ae1847eaa3b74b2186ca3ed3d8c
Trevor Pinto <74032637+roverTunleashed@users.noreply.github.com>
2025-06-27 23:18:27 -0700

    added basic header, q: class selector inheritance

--- src/HomePage/Header.js
+++ src/HomePage/Header.js
@@ -3,8 +3,15 @@ import image from '../RMSP.png'
 
 function DisplayHeader(){
     return <>
-        <img src = {image}/>
-        <span>About</span>
-        <h1>Learn More</h1>
+        <ul className="header">
+            <li>
+                <img src = {image}/>
+            </li>
+            <li>About</li>
+            <li>Link to my Github!</li>
+            <li>Learn More</li>
+        </ul>       
     </>
 }
+
+export default DisplayHeader
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // <--- MUST BE IMPORTED
+import { BrowserRouter } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
