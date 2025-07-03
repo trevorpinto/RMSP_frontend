3c87076b43957e92a126ef5d466d7445f496374a
Trevor Pinto <74032637+roverTunleashed@users.noreply.github.com>
2025-07-02 23:01:18 -0700

    created basic game with simple logic: q: eventHandlers, react syntax, DS for tournament style format

--- src/GameFolder/Deathmatch.js
+++ src/GameFolder/Deathmatch.js
@@ -3,12 +3,27 @@ import image from "../1655938361292.jpg"
 import imageTwo from "../mmonagan.img.-990583746.png"
 
 function DeathmatchUI(){
+
+    const handleBrianWin = () => {
+        alert('Winner is Brian!!');
+    };
+
+    const handleMichealWin = () => {
+        alert('Winner is Micheal!')
+    };
+
     return(<>
-        <img src={image}/>
+    <div className="dmContainer">
+        <img src={image} className="exampleOne"
+        onClick={handleBrianWin}
+        />
 
         <h1> VS </h1>
-
-        <img src = {imageTwo}/>
+        
+        <img src = {imageTwo} className="exampleTwo"
+            onClick={handleMichealWin}
+        />
+    </div>
 
     </>)
 }
